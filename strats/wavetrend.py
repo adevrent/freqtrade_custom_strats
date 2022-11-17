b307950d31fffa6dc4ea2a2f81243cfad68afd3f
adevrent <a.devrent@hotmail.com>
2022-11-17 19:09:24 +0300

    created a new strategy: wavetrend

---
+++ strats/wavetrend.py
@@ -0,0 +1,228 @@
+# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
+# flake8: noqa: F401
+# isort: skip_file
+# --- Do not remove these libs ---
+import numpy as np  # noqa
+import pandas as pd  # noqa
+from pandas import DataFrame
+
+from freqtrade.strategy import (BooleanParameter, CategoricalParameter, DecimalParameter,
+                                IStrategy, IntParameter)
+
+# --------------------------------
+# Add your lib to import here
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+
+
+# This class is a sample. Feel free to customize it.
+class WaveTrend(IStrategy):
+    """
+    This is a sample strategy to inspire you.
+    More information in https://www.freqtrade.io/en/latest/strategy-customization/
+
+    You can:
+        :return: a Dataframe with all mandatory indicators for the strategies
+    - Rename the class name (Do not forget to update class_name)
+    - Add any methods you want to build your strategy
+    - Add any lib you need to build your strategy
+
+    You must keep:
+    - the lib in the section "Do not remove these libs"
+    - the methods: populate_indicators, populate_entry_trend, populate_exit_trend
+    You should keep:
+    - timeframe, minimal_roi, stoploss, trailing_*
+    """
+    # Strategy interface version - allow new iterations of the strategy interface.
+    # Check the documentation or the Sample strategy to get the latest version.
+    INTERFACE_VERSION = 3
+
+    # Can this strategy go short?
+    can_short: bool = True
+
+    # Minimal ROI designed for the strategy.
+    # This attribute will be overridden if the config file contains "minimal_roi".
+    minimal_roi = {
+        "0": 0.1
+    }
+
+    # Optimal stoploss designed for the strategy.
+    # This attribute will be overridden if the config file contains "stoploss".
+    stoploss = -0.8
+
+    # Trailing stoploss
+    trailing_stop = False
+    trailing_only_offset_is_reached = True
+    trailing_stop_positive = 0.005
+    trailing_stop_positive_offset = 0.02  # Disabled / not configured
+
+    # Optimal timeframe for the strategy.
+    # timeframe = '5m'
+
+    # Run "populate_indicators()" only for new candle.
+    process_only_new_candles = True
+
+    # These values can be overridden in the config.
+    use_exit_signal = False
+    exit_profit_only = False
+    ignore_roi_if_entry_signal = False
+
+    # Hyperoptable parameters
+    #buy_rsi = IntParameter(low=1, high=50, default=30, space='buy', optimize=True, load=True)
+    #sell_rsi = IntParameter(low=50, high=100, default=70, space='sell', optimize=True, load=True)
+    #short_rsi = IntParameter(low=51, high=100, default=70, space='sell', optimize=True, load=True)
+    #exit_short_rsi = IntParameter(low=1, high=50, default=30, space='buy', optimize=True, load=True)
+
+    # Number of candles the strategy requires before producing valid signals
+    startup_candle_count: int = 40
+
+    # Optional order type mapping.
+    order_types = {
+        'entry': 'limit',
+        'exit': 'limit',
+        'stoploss': 'market',
+        'stoploss_on_exchange': False
+    }
+
+    # Optional order time in force.
+    order_time_in_force = {
+        'entry': 'gtc',
+        'exit': 'gtc'
+    }
+
+    plot_config = {
+        'main_plot': {
+            'tema': {},
+            'sar': {'color': 'white'},
+        },
+        'subplots': {
+            "MACD": {
+                'macd': {'color': 'blue'},
+                'macdsignal': {'color': 'orange'},
+            },
+            "RSI": {
+                'rsi': {'color': 'red'},
+            }
+        }
+    }
+
+    def informative_pairs(self):
+        """
+        Define additional, informative pair/interval combinations to be cached from the exchange.
+        These pair/interval combinations are non-tradeable, unless they are part
+        of the whitelist as well.
+        For more information, please consult the documentation
+        :return: List of tuples in the format (pair, interval)
+            Sample: return [("ETH/USDT", "5m"),
+                            ("BTC/USDT", "15m"),
+                            ]
+        """
+        return []
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Adds several different TA indicators to the given DataFrame
+
+        Performance Note: For the best performance be frugal on the number of indicators
+        you are using. Let uncomment only the indicator you are using in your strategies
+        or your hyperopt configuration, otherwise you will waste your memory and CPU usage.
+        :param dataframe: Dataframe with data from the exchange
+        :param metadata: Additional information, like the currently traded pair
+        :return: a Dataframe with all mandatory indicators for the strategies
+        """
+
+        
+        
+
+
+        ##########################################################################################################################
+        
+        # Momentum Indicators
+        # ------------------------------------
+
+        # Wave Trends
+        
+        n1 = 9  # Channel Length input.
+        n2 = 12  # Average Length input.
+        
+        dataframe["hlc3"] = ((dataframe["high"] + dataframe["low"] + dataframe["close"]) / 3)  # HLC Value initialized.
+        dataframe["esa"] = ta.EMA(dataframe["hlc3"], timeperiod = n1)
+        dataframe["abs_hlc3_esa"] = abs(dataframe["hlc3"] - dataframe["esa"])
+        dataframe["d"] = ta.EMA(dataframe["abs_hlc3_esa"], timeperiod = n1)
+        dataframe["ci"] = ((dataframe["hlc3"] - dataframe["esa"]) / (0.015 * dataframe["d"]))
+        dataframe["tci"] = ta.EMA(dataframe["ci"], n2)
+        
+        dataframe["wt1"] = dataframe["tci"]  # Wavetrend 1 (white wave)
+        dataframe["wt2"] = ta.SMA(dataframe["wt1"], 3)  # Wavetrend 2 (blue wave)
+        
+
+
+
+        # MFI
+        #dataframe['mfi'] = ta.MFI(dataframe)
+
+        # Retrieve best bid and best ask from the orderbook
+        # ------------------------------------
+        """
+        # first check if dataprovider is available
+        if self.dp:
+            if self.dp.runmode.value in ('live', 'dry_run'):
+                ob = self.dp.orderbook(metadata['pair'], 1)
+                dataframe['best_bid'] = ob['bids'][0][0]
+                dataframe['best_ask'] = ob['asks'][0][0]
+        """
+
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Based on TA indicators, populates the entry signal for the given dataframe
+        :param dataframe: DataFrame
+        :param metadata: Additional information, like the currently traded pair
+        :return: DataFrame with entry columns populated
+        """
+        dataframe.loc[
+            (
+                # Signal:
+                (qtpylib.crossed_above(dataframe["wt1"], dataframe["wt2"])) &  # wt1 crossed above wt2 aka green dot ...
+                (dataframe["wt2"] < -80) &  # While wavetrend is below -80 (oversold zone)
+                (dataframe['volume'] > 0)  # Make sure Volume is not 0
+            ),
+            'enter_long'] = 1
+
+        dataframe.loc[
+            (
+                # Signal:
+                (qtpylib.crossed_below(dataframe["wt1"], dataframe["wt2"])) &  # Candle high swept maxima.
+                (dataframe["wt2"] > 80) &  # While wavetrend is above 80 (overbought zone)
+                (dataframe['volume'] > 0)  # Make sure Volume is not 0
+            ),
+            'enter_short'] = 1
+
+        return dataframe
+
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Based on TA indicators, populates the exit signal for the given dataframe
+        :param dataframe: DataFrame
+        :param metadata: Additional information, like the currently traded pair
+        :return: DataFrame with exit columns populated
+        """
+        dataframe.loc[
+            (
+                # Signal: Candle high crosses above maxima point.
+                (qtpylib.crossed_below(dataframe["rsi"], 70)) &
+                (dataframe['volume'] > 0)  # Make sure Volume is not 0
+            ),
+
+            'exit_long'] = 1
+
+        dataframe.loc[
+            (
+                # Signal: Candle low crosses below minima point.
+                (qtpylib.crossed_above(dataframe["rsi"], 30)) &
+                (dataframe['volume'] > 0)  # Make sure Volume is not 0
+            ),
+            'exit_short'] = 1
+
+        return dataframe
